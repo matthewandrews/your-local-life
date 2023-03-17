8875ac882c80e805b200db90f6f053d6c84dd741
Matt Andrews <Matt.Andrews@csiro.au>
2023-03-17 11:05:00 +1100

    initial fade in

---
+++ your-local-life.js
@@ -0,0 +1,13 @@
+
+$(document).ready(function () {
+    $('.your-local-life-title').fadeIn(1500, function() {
+        $('.begin-para').fadeIn(1500, function() {
+            $('.where-para').fadeIn(1500, function() {
+                //
+            });
+        });
+    });
+});
+
+
+
